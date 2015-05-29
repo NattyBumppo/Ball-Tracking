cc1eb8ec429a9cc3b5b8dff1567478a1d20a29fc
Nathaniel M Guy <natguy@cs.washington.edu>
2015-05-29 00:43:22 -0700

    Fixed gravity--the height predictions look pretty perfect now. Started working on a green-tracking file which probably won't get used for anything. (Might delete later.)
    Former-commit-id: 94c9dd8255e80c01cd140582715fee998ff16a61

--- tracker_three_green.py
+++ tracker_three_green.py
@@ -1,6 +1,7 @@
 from __future__ import division
 import numpy as np
 import cv2
+from operator import itemgetter
 
 hsvColorBounds = {}
 hsvColorBounds['darkGreen'] = (np.array([35,50,20],np.uint8), np.array([80,255,120],np.uint8))
@@ -8,18 +9,7 @@ hsvColorBounds['green'] = (np.array([30,0,0],np.uint8), np.array([100,255,255],n
 hsvColorBounds['white'] = (np.array([0,0,80],np.uint8), np.array([255,50,120],np.uint8))
 hsvColorBounds['yellow'] = (np.array([15, 204, 204],np.uint8), np.array([20, 255, 255],np.uint8))
 hsvColorBounds['red'] = (np.array([0, 153, 127],np.uint8), np.array([4, 230, 179],np.uint8))
-
-pixelsPerMeter = 519.0 # Just a guess from looking at the video
-FPS = 30.0
-
-# Euler's method will proceed one frame at a time
-timeStepSize = 1.0 / FPS
-
-# Number of Euler's method steps to take
-eulerSteps = 20
-
-# Gravitational acceleration is in units of pixels per second squared
-g = 9.81 * pixelsPerMeter
+hsvColorBounds['light_green'] = (np.array([78, 77, 128],np.uint8), np.array([81, 217, 179],np.uint8))
 
 # Get a frame from the current video source
 def getFrame(cap):
@@ -77,21 +67,20 @@ def getContours(image):
 def estimateVelocity(pos0, pos1, normalized=False):
     if normalized:
         mag = np.sqrt((pos1[0] - pos0[0])**2 + (pos1[1] - pos0[1])**2)
-        velocity = [(pos1[0] - pos0[0]) / mag, (pos1[1] - pos0[1]) / mag]
+        velocity = ((pos1[0] - pos0[0]) / mag, (pos1[1] - pos0[1]) / mag)
     else:
-        velocity = [(pos1[0] - pos0[0]), (pos1[1] - pos0[1])]
+        velocity = ((pos1[0] - pos0[0]), (pos1[1] - pos0[1]))
     
     return velocity
 
 def main():
-
     ballCenters = [[0,0],[0,0],[0,0]]
     ballVelocities = [[0,0],[0,0],[0,0]]
 
-    cap = cv2.VideoCapture('juggling.mp4')
+    cap = cv2.VideoCapture('three_green.mp4')
 
     fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))
+    out = cv2.VideoWriter('three_greens_output.avi',fourcc, 20.0, (640,480))
     
     while(cap.isOpened()):
         frame = getFrame(cap)
@@ -108,12 +97,12 @@ def main():
         cv2.imshow('hsvBlurredFrame', hsvBlurredFrame)
 
         # Find locations of yellow balls
-        color = 'yellow'
+        color = 'light_green'
         colorBounds = hsvColorBounds[color]
         thresholdImage = cv2.inRange(hsvBlurredFrame, colorBounds[0], colorBounds[1])
 
         # Open to remove small elements/noise
-        kernel = np.ones((5,5)).astype(np.uint8)
+        kernel = np.ones((3,3)).astype(np.uint8)
         thresholdImage = cv2.erode(thresholdImage, kernel)
         thresholdImage = cv2.dilate(thresholdImage, kernel)
 
@@ -125,54 +114,59 @@ def main():
         if len(points[0]) >= 2:
             # Break into clusters using k-means clustering
             criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-            compactness, labels, centers = cv2.kmeans(points, 2, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)  
+            compactness, labels, centers = cv2.kmeans(points, 3, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)  
             # print compactness
 
 
             # This line helps things not crash, for some reason
             centers = centers.tolist()
 
-            # Don't let the blue and white marked balls get mixed up
+            # Don't let the balls get mixed up
             distBC0toC0 = np.sqrt((ballCenters[0][0] - centers[0][1])**2 + (ballCenters[0][1] - centers[0][0])**2)
             distBC0toC1 = np.sqrt((ballCenters[0][0] - centers[1][1])**2 + (ballCenters[0][1] - centers[1][0])**2)
+            distBC0toC2 = np.sqrt((ballCenters[0][0] - centers[2][1])**2 + (ballCenters[0][1] - centers[2][0])**2)
 
-            if distBC0toC0 < distBC0toC1:
-                # Ball 0 <-> Center 0, Ball 1 <-> Center 1
+            distBC1toC0 = np.sqrt((ballCenters[1][0] - centers[0][1])**2 + (ballCenters[1][1] - centers[0][0])**2)
+            distBC1toC1 = np.sqrt((ballCenters[1][0] - centers[1][1])**2 + (ballCenters[1][1] - centers[1][0])**2)
+            distBC1toC2 = np.sqrt((ballCenters[1][0] - centers[2][1])**2 + (ballCenters[1][1] - centers[2][0])**2)
 
-                # First find the velocity for the first ball and update the position
-                ballVelocities[0] = estimateVelocity(ballCenters[0], (centers[0][1], centers[0][0]))
-                ballCenters[0] = (int(centers[0][1]), int(centers[0][0]))
+            correspondingCenters = []
 
-                # Now find the velocity for the second ball and update the position
-                ballVelocities[1] = estimateVelocity(ballCenters[1], (centers[1][1], centers[1][0]))
-                ballCenters[1] = (int(centers[1][1]), int(centers[1][0]))
-            else:
-                # Ball 1 <-> Center 0, Ball 0 <-> Center 1
+            # Assign ball 0
+            ballDistances = [distBC0toC0, distBC0toC1, distBC0toC2]
+            BC0CorrespondingCenter = min(enumerate(ballDistances), key=itemgetter(1))[0]
+            correspondingCenters.append(BC0CorrespondingCenter)
 
-                # First find the velocity for the first ball and update the position
-                ballVelocities[1] = estimateVelocity(ballCenters[1], (centers[0][1], centers[0][0]))
-                ballCenters[1] = (int(centers[0][1]), int(centers[0][0]))
+            # Assign ball 1
+            ballDistances = [distBC1toC0, distBC1toC1, distBC1toC2]
+            BC1CorrespondingCenter = min(enumerate(ballDistances), key=itemgetter(1))[0]
+            # Adjust if this index is already taken
+            if (BC1CorrespondingCenter == BC0CorrespondingCenter):
+                BC1CorrespondingCenter = min(enumerate(ballDistances[:BC1CorrespondingCenter-1]+ballDistances[BC1CorrespondingCenter:]), key=itemgetter(1))[0]
+            correspondingCenters.append(BC1CorrespondingCenter)
 
-                # Now find the velocity for the second ball and update the position
-                ballVelocities[0] = estimateVelocity(ballCenters[0], (centers[1][1], centers[1][0]))
-                ballCenters[0] = (int(centers[1][1]), int(centers[1][0]))
+            # Give the last one to ball 2
+            BC2CorrespondingCenter = [x for x in [0, 1, 2] if x not in [BC0CorrespondingCenter, BC1CorrespondingCenter]][0]
+            correspondingCenters.append(BC2CorrespondingCenter)
+
+            # First find the velocity for each ball and update the position
+            for i in range(3):
+                ballVelocities[i] = estimateVelocity(ballCenters[0], (centers[correspondingCenters[i]][1], centers[correspondingCenters[i]][0]))
+                ballCenters[i] = (int(centers[correspondingCenters[i]][1]), int(centers[correspondingCenters[i]][0]))                
+
+
 
             # Draw position markers
             cv2.circle(frame, tuple(ballCenters[0]), 6, (200,0,0), thickness=6)
             cv2.circle(frame, tuple(ballCenters[1]), 6, (200,200,200), thickness=6)
+            cv2.circle(frame, tuple(ballCenters[2]), 6, (100,200,0), thickness=6)
 
             # Draw velocity vectors
             # cv2.arrowedLine(frame, tuple(ballCenters[0]), (int(ballCenters[0][0]+ballVelocities[0][0]*2), int(ballCenters[0][1]+ballVelocities[0][1]*2)), (255,0,0), 2, 2, 0, 0.1)
             # cv2.arrowedLine(frame, tuple(ballCenters[1]), (int(ballCenters[1][0]+ballVelocities[1][0]*2), int(ballCenters[1][1]+ballVelocities[1][1]*2)), (255,255,255), 2, 2, 0, 0.1)
 
-            # Adjust velocities to be in pixels/sec
-            ballVelocities[0][0] = ballVelocities[0][0] / timeStepSize
-            ballVelocities[0][1] = ballVelocities[0][1] / timeStepSize
-            ballVelocities[1][0] = ballVelocities[1][0] / timeStepSize
-            ballVelocities[1][1] = ballVelocities[1][1] / timeStepSize
 
-            positions = getTrajectory(ballCenters[0], ballVelocities[0], (0, g), timeStepSize, eulerSteps)
-
+            positions = getTrajectory(ballCenters[0], ballVelocities[0], (0, 9.81), 0.100, 60)
             # print positions
             # print ''
 
@@ -188,7 +182,7 @@ def main():
                                     
                     cv2.circle(frame, (int(position[0]), int(position[1])), 2, ballColor, thickness=2)
 
-            positions = getTrajectory(ballCenters[1], ballVelocities[1], (0, g), timeStepSize, eulerSteps)
+            positions = getTrajectory(ballCenters[1], ballVelocities[1], (0, 9.81), 0.100, 60)
 
             for i, position in enumerate(positions):
                 height, width, depth = frameCopy.shape
@@ -202,45 +196,7 @@ def main():
                                     
                     cv2.circle(frame, (int(position[0]), int(position[1])), 2, ballColor, thickness=2)                    
 
-        # points = rejectOutlierPoints(points)
-
-        # Find location of red ball
-        color = 'red'
-        colorBounds = hsvColorBounds[color]
-        thresholdImage = cv2.inRange(hsvBlurredFrame, colorBounds[0], colorBounds[1])
-
-        # Open to remove small elements/noise
-        kernel = np.ones((5,5)).astype(np.uint8)
-        thresholdImage = cv2.erode(thresholdImage, kernel)
-        thresholdImage = cv2.dilate(thresholdImage, kernel)
-
-        # Find the points in the image where this is true
-        points = np.dstack(np.where(thresholdImage>0)).astype(np.float32)
-
-        if len(points[0]) >= 1:
-            # Break into clusters using k-means clustering
-            criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-            compactness, labels, centers = cv2.kmeans(points, 1, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
-            
-            # This line helps things not crash, for some reason
-            centers = centers.tolist()
-
-            # Find the velocity for the third ball and update its position
-            ballVelocities[2] = estimateVelocity(ballCenters[2], (centers[0][1], centers[0][0]))
-            ballCenters[2] = (int(centers[0][1]), int(centers[0][0]))
-
-            # Adjust velocity to be in pixels/sec
-            ballVelocities[2][0] = ballVelocities[2][0] / timeStepSize
-            ballVelocities[2][1] = ballVelocities[2][1] / timeStepSize
-
-            # Draw position marker
-            cv2.circle(frame, tuple(ballCenters[2]), 6, (50,200,50), thickness=6)
-            # Draw velocity vector
-            # cv2.arrowedLine(frame, tuple(ballCenters[2]), (int(ballCenters[2][0]+ballVelocities[2][0]*2), int(ballCenters[2][1]+ballVelocities[2][1]*2)), (0,255,255), 2, 2, 0, 0.1)
-
-            positions = getTrajectory(ballCenters[2], ballVelocities[2], (0, g), timeStepSize, eulerSteps)
-            # print positions
-            # print ''
+            positions = getTrajectory(ballCenters[2], ballVelocities[2], (0, 9.81), 0.100, 60)
 
             for i, position in enumerate(positions):
                 height, width, depth = frameCopy.shape
@@ -250,15 +206,14 @@ def main():
                     # blankImageAlpha = cv2.cvtColor(blankImage, cv2.COLOR_BGR2RGBA)
                     # # Alpha blending depending on how far along this is
                     # alpha = i / len(positions)
-                    ballColor = (105,255,105)
+                    ballColor = (155,255,55)
                                     
-                    cv2.circle(frame, (int(position[0]), int(position[1])), 2, ballColor, thickness=2)
+                    cv2.circle(frame, (int(position[0]), int(position[1])), 2, ballColor, thickness=2)       
 
 
         cv2.imshow('Image with Estimated Ball Center', frame)
         out.write(frame)
-
-        k = cv2.waitKey(int(1000.0 / FPS)) & 0xFF
+        k = cv2.waitKey(33) & 0xFF
         if k == 27:
             # User hit ESC
             break
